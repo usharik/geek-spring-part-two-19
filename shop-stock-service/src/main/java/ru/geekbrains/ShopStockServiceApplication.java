494af0f35c470953948609ed28570632d1c08ba8
usharik <usharik@mail.ru>
2020-11-30 20:24:21 +0100

    Lesson-6 rabbitMQ spring integration

---
+++ shop-stock-service/src/main/java/ru/geekbrains/ShopStockServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopStockServiceApplication.class, args);
+	}
+
+}
